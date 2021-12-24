fc9246615b73549531edc03a2601674267a1631c
Pratham <pgupta4596@gmail.com>
2021-12-24 15:09:00 +0530

    Updates

---
+++ precache-manifest.765b02c545cb4cdcc02508af0192fe5a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "336416f9247c25ab7792",
+    "url": "/TextUtils/static/css/main.5544f8d2.chunk.css"
+  },
+  {
+    "revision": "336416f9247c25ab7792",
+    "url": "/TextUtils/static/js/main.336416f9.chunk.js"
+  },
+  {
+    "revision": "e88f3e01182016b2c5ca",
+    "url": "/TextUtils/static/js/1.e88f3e01.chunk.js"
+  },
+  {
+    "revision": "3a2e0e0b763789a1abbb",
+    "url": "/TextUtils/static/js/2.3a2e0e0b.chunk.js"
+  },
+  {
+    "revision": "f6ed396c11cf11f62199",
+    "url": "/TextUtils/static/js/runtime~main.f6ed396c.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/TextUtils/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "4c15e3dd1756779c8752410a5c5667bf",
+    "url": "/TextUtils/index.html"
+  }
+];
